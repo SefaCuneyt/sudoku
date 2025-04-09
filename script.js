7476c478ee21a063a035ffec80c00a9f75895578
SefaCuneyt <97847430+SefaCuneyt@users.noreply.github.com>
2025-04-10 01:37:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const sudokuBoard = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+  ];
+  
+  const table = document.querySelector('.sudoku-board table');
+  
+  for (let i = 0; i < 9; i++) {
+    const row = document.createElement('tr'); 
+    for (let j = 0; j < 9; j++) {
+      const cell = document.createElement('td'); 
+      const input = document.createElement('input');
+      input.type = 'text'; 
+      input.maxLength = 1; 
+      input.classList.add('sudoku-cell'); 
+      input.value = sudokuBoard[i][j]; 
+      cell.appendChild(input); 
+      row.appendChild(cell); 
+    }
+    table.querySelector('tbody').appendChild(row); 
+  }
+  
